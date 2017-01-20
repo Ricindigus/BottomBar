6aa15251f09c91d4115fbb66f70f2d5096b707f2
Ricardo <ricindigus@gmail.com>
2017-01-20 11:37:09 -0500

    Actualizado scroll pantalla Sancionado

--- app/src/main/java/com/example/ricardo/rougikebottombar/MainActivity.java
+++ app/src/main/java/com/example/ricardo/rougikebottombar/MainActivity.java
@@ -1,45 +1,117 @@
 package com.example.ricardo.rougikebottombar;
 
 import android.support.annotation.IdRes;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
-import com.example.ricardo.rougikebottombar.fragments.DeporteFragment;
-import com.example.ricardo.rougikebottombar.fragments.SaludFragment;
+import com.example.ricardo.rougikebottombar.fragments.Fragment1;
+import com.example.ricardo.rougikebottombar.fragments.Fragment2;
+import com.example.ricardo.rougikebottombar.fragments.Fragment3;
+import com.example.ricardo.rougikebottombar.fragments.Fragment4;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
+    int[] fragments = {R.id.tab_1, R.id.tab_2, R.id.tab_3, R.id.tab_4};
+    int fragmentActual = 0;
+    int fragmentAnterior = 0;
+    Fragment fragment;
+    BottomBar bottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setDefaultTab(R.id.tab_deporte);
-
-
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setDefaultTab(fragments[fragmentActual]);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_deporte) {
-                    DeporteFragment deporteFragment = new DeporteFragment();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                switch(tabId){
+                    case R.id.tab_1:
+                        fragmentAnterior = fragmentActual;
+                        fragmentActual = 0;
+                        fragment = new Fragment1();
+                        break;
+                    case R.id.tab_2:
+                        fragmentAnterior = fragmentActual;
+                        fragmentActual = 1;
+                        fragment = new Fragment2();
+                        break;
+                    case R.id.tab_3:
+                        fragmentAnterior = fragmentActual;
+                        fragmentActual = 2;
+                        fragment = new Fragment3();
+                        break;
+                    case R.id.tab_4:
+                        fragmentAnterior = fragmentActual;
+                        fragmentActual = 3;
+                        fragment = new Fragment4();
+                        break;
+                    default:break;
+                }
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                if(fragmentActual > fragmentAnterior)
                     fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
-                    fragmentTransaction.replace(R.id.contentContainer,deporteFragment);
-                    fragmentTransaction.addToBackStack(null).commit();
-                }
-                if (tabId == R.id.tab_salud) {
-                    SaludFragment saludFragment = new SaludFragment();
-                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction1.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
-                    fragmentTransaction1.replace(R.id.contentContainer,saludFragment);
-                    fragmentTransaction1.addToBackStack(null).commit();
-                }
+                else
+                    fragmentTransaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_left);
+                fragmentTransaction.replace(R.id.contentContainer, fragment);
+                fragmentTransaction.addToBackStack(null).commit();
+
+//                if (tabId == fragments[1]) {
+//                    fragmentActual = 1;
+//                    Fragment3 fragment3 = new Fragment3();
+//                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+//                    fragmentTransaction1.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right);
+//                    fragmentTransaction1.replace(R.id.contentContainer, fragment3);
+//                    fragmentTransaction1.addToBackStack(null).commit();
+//                }
 
             }
         });
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    // Left to Right swipe action
+                    if (x2 > x1)
+                    {
+                        if(fragmentActual > 0){
+                            bottomBar.selectTabAtPosition(fragmentActual-1);
+                        }
+                        Toast.makeText(this,(fragmentActual-1)+"" , Toast.LENGTH_SHORT).show ();
+                    }
+                    // Right to left swipe action
+                    else
+                    {
+                        if(fragmentActual < 3) {
+                            bottomBar.selectTabAtPosition(fragmentActual+1);
+                        }
+                        Toast.makeText(this,(fragmentActual+1)+""  , Toast.LENGTH_SHORT).show ();
+                    }
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
